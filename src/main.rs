0de2c1f9f4b836374706667d104ca25f22eadbd7
thatcomputerguy0101 <thatcomputerguy0101@gmail.com>
2024-09-17 14:26:24 -0400

    Unhide cursor upon exit

--- src/main.rs
+++ src/main.rs
@@ -904,6 +904,7 @@ fn main_inner() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+    stdout.execute(cursor::Show)?;
     stdout.execute(terminal::LeaveAlternateScreen)?;
     terminal::disable_raw_mode()?; // does this help?
     Ok(())
